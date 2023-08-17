97d469961445a158130401ce6135dff53f14e678
Emre Şanlı <memres@gmail.com>
2023-08-17 21:34:19 +0300

    Update related.php

--- related.php
+++ related.php
@@ -1,48 +1,29 @@
 <?php
 if (isset($_SERVER['HTTP_REFERER']) && parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST) == $_SERVER['SERVER_NAME'] && !empty($_GET['id'])) {
-	include('functions.php');
-	$pm += array(
-		'relatedToVideoId' => $_GET['id'],
-		'relevanceLanguage' => $language,
-		'safeSearch' => $safe,
-		'maxResults' => 20,
-		'part' => 'snippet',
-		'type' => 'video',
-		'fields' => 'nextPageToken,items(id(videoId))',
-		'pageToken' => !empty($_GET['token']) ? $_GET['token'] : null
-	);
-	$ch = curl_init($api.'search?'.http_build_query($pm));
+	$ch = curl_init('https://api-piped.mha.fi/streams/'.$_GET['id']);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)');
 	$result = curl_exec($ch);
 	curl_close($ch);
 	$json = json_decode($result, true);
-	if (empty($json['items'])) echo "\n";
+	//$json = json_decode(file_get_contents('https://api-piped.mha.fi/streams/'.$_GET['id']), true);
+	if (empty($json['relatedStreams'])) echo "\n";
 	else {
-		foreach ($json['items'] as $item) $ids[] = $item['id']['videoId'];
-		$pms += array(
-			'id' => implode(',', $ids),
-			'part' => 'snippet,contentDetails',
-			'fields' => 'items(id,snippet(title,channelTitle),contentDetails(duration))'
-		);
-		$ch = curl_init($api.'videos?'.http_build_query($pms));
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-		$result = curl_exec($ch);
-		curl_close($ch);
-		$json2 = json_decode($result, true);
-		foreach ($json2['items'] as $item) {
+		foreach ($json['relatedStreams'] as $item) {
+			$h = floor($item['duration'] / 3600);
+			$m = floor(($item['duration'] - $h * 3600) / 60);
+			$s = $item['duration'] - ($h * 3600 + $m * 60);
+			$m = ($m < 10 ? '0'.$m : ''.$m);
+			$s = ($s < 10 ? '0'.$s : ''.$s);
+			if (strpos($item['url'], $_GET['id']) === false) {
 ?>
 					<article>
-						<a href="?v=<?= $item['id']; ?>">
-							<small><?= $item['snippet']['channelTitle']; ?></small>
-							<span><?= $item['snippet']['title']; ?></span>
-							<img src="https://i.ytimg.com/vi/<?= $item['id']; ?>/mqdefault.jpg" alt="<?= htmlspecialchars($item['snippet']['title'], ENT_QUOTES); ?>"/>
+						<a href="?v=<?= $id = str_replace('/watch?v=', '', $item['url']); ?>">
+							<small><?= $item['uploaderName']; ?></small>
+							<span><?= $item['title']; ?></span>
+							<img src="https://i.ytimg.com/vi/<?= $id; ?>/mqdefault.jpg" alt="<?= htmlspecialchars($item['title'], ENT_QUOTES); ?>"/>
 						</a>
-						<b id="<?= $item['id']; ?>"><?= duration($item['contentDetails']['duration']); ?> <i class="fas fa-plus-circle"></i></b>
+						<b id="<?= $id; ?>"><?= ($h > 0 ? $h.':' : '').$m.':'.$s; ?> <i class="fas fa-plus-circle"></i></b>
 					</article>
-<?php } if (!empty($json['nextPageToken'])) { ?>
-					<aside id="<?= $json['nextPageToken']; ?>">
-						<a class="more"><i class="fas fa-sync-alt"></i> <?= $l10n['more']; ?></a>
-					</aside>
-<?php } } } ?>
+<?php } } } } ?>
